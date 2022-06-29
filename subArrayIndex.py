076c4a534e645a087c803c3674d1b688c5351322
rspraneeth <rspraneeth.rsp@gmail.com>
2022-06-29 23:15:54 +0530

    max sub array of length k

--- subArrayIndex.py
+++ subArrayIndex.py
@@ -1,7 +1,13 @@
 k = 3
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = [10, 20, 3, 4, 5, 6, 7, 8, 9, 10]
 n = len(a)
-for i in range(n-k+1):
+for i in range(n-k+1):  # n-k+1 sub arrays
     print(i, i+k-1)
 
-#  n-k+1 sub arrays
+# return max sum of a sub array of length k, shouldn't use prefix sum, shouldn't modify array, T: O(n), S: O(1)
+ans = sum(a[:k])
+sum = ans
+for i in range(1, n-k+1):
+    sum = sum - a[i-1] + a[i+2]
+    ans = max(ans, sum)
+print(ans)
