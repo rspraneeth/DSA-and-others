a64b22eaa6ee79566777b5624e24454481b91876
rspraneeth <rspraneeth.rsp@gmail.com>
2022-07-01 13:06:56 +0530

    spiral matrix printing

--- spiralMatrix.py
+++ spiralMatrix.py
@@ -1,7 +1,8 @@
-# we are generating spiral array after we iterate through the top row we need not go through it again, so we increase
-# top row value by 1, similarly we decrease right column by 1, increase left col by 1, decrease bottom row by 1.
-# We change these values of top, right, bottom, left so that we don't iterate through same rows/columns
-def generateMatrix(A):
+
+def generateMatrix1(A):
+    """we are generating spiral array after we iterate through the top row we need not go through it again, so we increase
+    top row value by 1, similarly we decrease right column by 1, increase left col by 1, decrease bottom row by 1.
+    We change these values of top, right, bottom, left so that we don't iterate through same rows/columns"""
     mat = [[0 for i in range(A)] for j in range(A)]
     top = 0
     left = 0
@@ -33,10 +34,40 @@ def generateMatrix(A):
             mat[x][y] = g
             g += 1
         left += 1  # after iterating through left column, we change its value to next column
-
     return mat
 
-print(generateMatrix(5))
+def generateMatrix2(A):
+    """Another approach, considering when A = 5"""
+    i, j, g = 0, 0, 1
+    n = A
+    mat = [[0 for i in range(A)] for j in range(A)]
+    while A > 1:
+        for k in range(A-1):  # for first A-1 = 4 elements of i = 0 row
+            mat[i][j] = g
+            g += 1
+            j += 1
+        for k in range(A-1):  # for first A-1 = 4 elements of j = 4 column
+            mat[i][j] = g
+            g += 1
+            i += 1
+        for k in range(A-1, 0, -1):  # for last A-1 = 4 elements of i = 4 row
+            mat[i][j] = g
+            g += 1
+            j -= 1
+        for k in range(A-1, 0, -1):  # for last A-1 = 4 elements of j = 0 column
+            mat[i][j] = g
+            g += 1
+            i -= 1
+        i += 1
+        j += 1
+        A -= 2
+    if A == 1:  # now we need to only assign for middle element, this cond comes into picture when A is odd
+        mat[i][j] = g
+    return mat
+
+
+print(generateMatrix1(6))
+print(generateMatrix2(6))
 
 """Given an integer A, generate a square matrix filled with elements from 1 to A2 in spiral order. 
 Problem Constraints
