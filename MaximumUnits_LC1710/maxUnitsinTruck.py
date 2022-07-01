5c0bd13324096b70dd34012193eff20b0ac2b6f1
rspraneeth <rspraneeth.rsp@gmail.com>
2022-07-01 11:56:50 +0530

    leet code 1710. Maximum Units on a Truck

---
+++ MaximumUnits_LC1710/maxUnitsinTruck.py
@@ -0,0 +1,20 @@
+A = [[1, 3], [2, 2], [3, 1]]
+B = 4
+
+def maximumUnits(boxTypes, truckSize):
+    boxTypes.sort(key=lambda x: x[1])
+    boxTypes.reverse()
+    i = 0
+    mx = 0
+    while truckSize > 0 and i < len(boxTypes):
+        if boxTypes[i][0] <= truckSize:
+            mx += boxTypes[i][0] * boxTypes[i][1]
+            truckSize -= boxTypes[i][0]
+        else:
+            mx += truckSize * boxTypes[i][1]
+            truckSize = 0
+        i += 1
+    return mx
+
+
+print(maximumUnits(A, B))
