b1011627588257c807fffae13d66563308ded848
rspraneeth <rspraneeth.rsp@gmail.com>
2022-07-04 22:41:12 +0530

    modular problems

--- modularArithmetic/modular.py
+++ modularArithmetic/modular.py
@@ -9,7 +9,9 @@ def titleToNumber(A):
         res += v * 26 ** (n - 1 - i)
     return res
 
+
 print(titleToNumber('ABCD'))
+
 
 # Given two integers A and B, find the greatest possible positive integer M, such that A % M = B % M.
 def greatest(A, B):
@@ -18,5 +20,28 @@ def greatest(A, B):
     else:
         return B - A
 
+
 print(greatest(6, 2))
 
+
+# LCM
+def LCM(A, B):
+    if A % B == 0 or B % A == 0:
+        return max(A, B)
+    mx = max(A, B)
+    mn = min(A, B)
+    for i in range(2, mx + 1):
+        mul = i * mn
+        if mul % mx == 0:
+            return mul
+
+
+print(LCM(3, 104))
+
+# Eight integers A, B, C, D, E, F, G, and H represent two rectangles in a 2D plane. For the first rectangle,
+# its bottom left corner is (A, B), and the top right corner is (C, D), and for the second rectangle, its bottom left
+# corner is (E, F), and the top right corner is (G, H). Return 1 if the two rectangles overlap else, return 0.
+def solve(A, B, C, D, E, F, G, H):
+    return (1, 0)[F >= D or H <= B or E >= C or G <= A]  # condition checks for rectangles side by side or up and down
+
+print(solve(0, 10, 10, 0, 5, 5, 15, 0))
