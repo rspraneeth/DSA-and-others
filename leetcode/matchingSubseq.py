3109d7d6ffa10567dcc061bd3ca0834b00b37ecb
rspraneeth <rspraneeth.rsp@gmail.com>
2022-07-20 14:50:24 +0530

    leet code 792, number of matching subsequences

--- leetcode/matchingSubseq.py
+++ leetcode/matchingSubseq.py
@@ -24,7 +24,7 @@ def numMatchingSubseq(s, words):
 
 print(numMatchingSubseq(s1, words1))
 
-# 792. Number of Matching Subsequences
+# Leet code 792. Number of Matching Subsequences
 # Given a string s and an array of strings words, return the number of words[i] that is a subsequence of s.
 # A subsequence of a string is a new string generated from the original string with some characters (can be none)
 # deleted without changing the relative order of the remaining characters.
