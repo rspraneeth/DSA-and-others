f14df2655547fa7d3d4b3dd101e501743dedbc61
rspraneeth <rspraneeth.rsp@gmail.com>
2022-07-21 16:40:36 +0530

    check if there exist a subarray with sum = 0

---
+++ Arrays/subArrSumZero.py
@@ -0,0 +1,23 @@
+# a = [1,3,4,6,6,3,1,2,4,6,8,9,5,2,1,2,3,3,4,5,6,8,5,1,1,2,3,4,5]
+# a = [1,2,5,6,8,0,4]
+a = [5, 1, 3, -5]
+# check if all elements in given array are distinct
+def checkAllDistinct(arr):
+    """if length of hash set equal to length of array means all are distinct elements
+    if not there exists repeated elements"""
+
+    return (False, True)[len(set(arr)) == len(arr)]
+
+print(checkAllDistinct(a))
+
+# check if there exist a subarray with sum = 0
+def subArrSumZero(arr):
+    """if there exists repetitive elements in prefix array, then there exists a subarray with sum zero. (take e.g.)"""
+    pf = [0] * len(arr)  # prefix array
+    pf[0] = arr[0]
+    for i in range(1, len(arr)):
+        pf[i] = pf[i-1] + arr[i]
+
+    return (False, True)[len(set(pf)) != len(arr) or 0 in pf]
+
+print(subArrSumZero(a))
